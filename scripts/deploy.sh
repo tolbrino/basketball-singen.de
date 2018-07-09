580ac56c438ec9276b298e9bdb0fb3e9be3e2e8c
Tino Breddin <t@tolbrino.com>
2018-07-09 18:28:25 +0200

    change travis deploy url

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ echo "Target: gh-pages branch"
 TEMP_DIRECTORY="/tmp/__temp_static_content"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
+ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/https\:\/\/github.com\//git@github.com:}
 
 echo "Compiling new static content"
 rm -rf $TEMP_DIRECTORY || exit 1
@@ -28,7 +29,7 @@ git config user.email "noreply@travisci.org" || exit 1
 
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-git push --force "$ORIGIN_URL" gh-pages || exit 1
+git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages || exit 1
 
 echo "Cleaning up temp files"
 rm -Rf $TEMP_DIRECTORY
